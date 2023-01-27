f784288b8832e0125101eaa852a2107a4aa64287
Caro Appleby <caroline.appleby@cantab.net>
2023-01-27 21:27:11 +0000

    dark theme

--- src/components/Links.svelte
+++ src/components/Links.svelte
@@ -24,12 +24,13 @@
 
 <style lang="postcss">
   p {
-    @apply  shadow-md p-2 transition-all text-xl duration-500 border-4 border-violet-800 rounded-md bg-slate-100;
-    @apply  hover:transition-all hover:bg-slate-50 focus:bg-slate-50 hover:shadow-xl focus:shadow-xl;
+    @apply text-slate-800 dark:text-slate-200 shadow-md p-2 transition-all text-xl duration-500 border-4 border-violet-800 dark:border-violet-300 rounded-md bg-slate-100 dark:bg-slate-800;
+    @apply  hover:transition-all hover:bg-slate-50 dark:hover:bg-slate-700 dark:focus:bg-slate-700 focus:bg-slate-50 hover:shadow-xl focus:shadow-xl;
   }
   
   :global(.stats) {
-    @apply text-sm text-violet-800 block;
+    @apply block text-sm;
+    @apply dark:text-violet-200 text-violet-800;
   }
   nav {
     @apply flex flex-col gap-4 max-w-prose w-96 mx-auto text-slate-800;
